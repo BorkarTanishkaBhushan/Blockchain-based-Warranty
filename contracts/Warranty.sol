6aef07457bc02107003668aed21e8831258e05ce
Tanishka Borkar <tanishka.b@somaiya.edu>
2022-07-13 09:54:21 -0200

    register transaction

--- contracts/Warranty.sol
+++ contracts/Warranty.sol
@@ -51,4 +51,6 @@ contract Warranty  {
         emit delivered(_productId);
         
     }
+
+    
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,17 @@
 const main = async () => {
 
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const warrantyContractFactory = await hre.ethers.getContractFactory("Warranty");
     const warrantyContract = await warrantyContractFactory.deploy();
     await warrantyContract.deployed();
     console.log("Contract deployed to:", warrantyContract.address);
+    console.log("Contract deployed by:", owner.address);
 
+    //calling contract functions
+    const r_tx = await warrantyContract.registerProduct("Krishna Idol", "Golden and black in color", "7189", "20");
+    await r_tx.wait();
+
+    
 }
 
 const runMain = async () => {
